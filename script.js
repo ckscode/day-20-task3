9459abdb1d9a9c34f601475eae7fe8082a7b8f82
ckscode <cksiddhaarthan@gmail.com>
2024-04-29 22:03:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//selecting all the HTML elements with DOM
+const InputItem = document.getElementById("location");
+const Btn = document.getElementById("click");
+const deg = document.getElementById("deg");
+const sky = document.getElementById("sky");
+
+//function to fetch data and set it to the selected HTML elements
+const FetchData = () => {
+  const Url = `https://api.openweathermap.org/data/2.5/weather?q=${InputItem.value}&appid=7045ff42f23d49e7601563eb17b37bfd&units=metric`;
+  fetch(Url)
+    .then((e) => e.json())
+    .then((item) => {
+      //setting to HTML elements
+      deg.innerHTML = `${Math.round(
+        item.main.temp
+      )}<span class="fw-light">°C</span>`;
+      sky.innerHTML = `${item.weather[0].main}`;
+    })
+    .catch((err) => {console.log(err)
+        sky.innerHTML = "give a proper input"});
+};
+
+//search button click and press enter to call the FetchData function
+Btn.addEventListener("click", () => FetchData());
+InputItem.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    FetchData();
+  }
+});
